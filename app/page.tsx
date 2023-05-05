00e4a5ffb43b5d621f8d89d8c7679c862f1f0f78
chadittya <chadittya@gmail.com>
2023-05-05 17:12:31 +0700

    finishing project

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,100 @@
-import Image from 'next/image'
+"use client";
+
+import Image from "next/image";
+import logo from "../public/logo.svg";
+import illustration from "../public/images/illustration-dashboard.png";
+import { FaFacebookF, FaTwitter, FaInstagram } from "react-icons/fa";
+import React, { useState } from "react";
+
+function validateEmail(email: string) {
+  const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return re.test(String(email).toLowerCase());
+}
 
 export default function Home() {
+  const [email, setEmail] = useState<string>("");
+  const [errorCopy, setErrorCopy] = useState<string>("");
+  const [hasErrors, setHasErrors] = useState<boolean>(false);
+
+  const handleSubmit: React.FormEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault();
+
+    if (!validateEmail(email)) {
+      setErrorCopy("Please Provide a valid email address");
+      setHasErrors(true);
+    } else {
+      setErrorCopy("");
+      setHasErrors(false);
+      console.log("Email has been submitted");
+    }
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main>
+      <div className="flex flex-col justify-center items-center gap-7 h-screen">
+        <div>
+          <Image src={logo} alt="logo" />
+        </div>
+        <div className="flex flex-col gap-3">
+          <h1 className="text-slate-400 md:text-4xl text-xl text-center">
+            We are launching{" "}
+            <span className="font-semibold text-black">soon!</span>
+          </h1>
+          <p className="text-center md:text-base text-xs">
+            Subscribe and get notified
+          </p>
+        </div>
+        <div className="w-2/3">
+          <form onSubmit={handleSubmit} className="w-full">
+            <div className="w-full grid md:grid-cols-3 md:grid-rows-2 grid-rows-3 md:px-36 gap-2">
+              <input
+                className={`col-span-2 rounded-full px-5 py-2 text-sm w-full outline-none focus:border-blue-500 ${
+                  hasErrors
+                    ? "border-2 border-red-400"
+                    : "border border-chPaleBlue"
+                }`}
+                type="text"
+                placeholder="Your email address..."
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <button
+                className="bg-chBlue rounded-full px-8 py-2 text-sm text-white font-light shadow-[0px_5px_10px_0px_#c2d3ff] hover:opacity-80 transition col-span-2 md:col-start-3"
+                type="submit"
+              >
+                Notify Me
+              </button>
+              {hasErrors && (
+                <p className="col-span-2 row-start-2 md:text-[12px] text-[11px] italic text-red-500 ml-5">
+                  {errorCopy}
+                </p>
+              )}
+            </div>
+          </form>
+        </div>
+        <div>
+          <Image src={illustration} alt="illustration dashboard" width={600} />
+        </div>
+        <div className="flex flex-row gap-2">
+          <FaFacebookF
+            className="border rounded-full px-2 py-2 cursor-pointer text-[#4f7df3] hover:bg-chBlue hover:text-white transition"
+            size={35}
+          />
+          <FaTwitter
+            className="border rounded-full px-2 py-2 cursor-pointer text-[#4f7df3] hover:bg-chBlue hover:text-white transition"
+            size={35}
+          />
+          <FaInstagram
+            className="border rounded-full px-2 py-2 cursor-pointer text-[#4f7df3] hover:bg-chBlue hover:text-white transition"
+            size={35}
+          />
+        </div>
+        <div>
+          <p className="text-[12px] text-slate-400">
+            &copy; Copyright Ping. All rights reserved.
+          </p>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
-  )
+  );
 }
---
+++ components/Footer.tsx
@@ -0,0 +1,27 @@
+const Footer = () => {
+  return (
+    <footer>
+      <p className="text-slate-400 text-center text-sm">
+        Challenge by{" "}
+        <a
+          className="font-semibold hover:text-black transition"
+          href="https://www.frontendmentor.io?ref=challenge"
+          target="_blank"
+        >
+          Frontend Mentor
+        </a>
+        . Coded by{" "}
+        <a
+          className="font-semibold hover:text-black transition"
+          href="https://www.frontendmentor.io/profile/chadittya"
+          target="_blank"
+        >
+          Galih Arizza
+        </a>
+        .
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
